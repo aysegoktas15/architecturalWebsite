9dc37b115b806052b8d1dfea887cfd5d7f34d958
Ayşe <aysegoktas727@gmail.com>
2024-08-02 12:32:39 +0300

    SideBar-1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "220px";
+    document.getElementById("main").style.marginLeft = "220px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
