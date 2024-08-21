d53addbf97336c94151c12e2c0cc332b29ae4045
Ayşe <aysegoktas727@gmail.com>
2024-08-21 11:08:41 +0300

    Sidebar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("sidebar").style.width = "240px";
-    document.getElementById("main").style.marginLeft = "240px";
+    document.getElementById("sidebar").style.width = "480px";
+    document.getElementById("main").style.marginLeft = "480px";
     document.getElementById("menuBtn").style.display = "none"
     document.getElementById("logoBtn").style.display = "none"
 }
